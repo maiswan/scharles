b681780c66fa6da99473f6ab3c198f11c90df8f7
maiswan <173935047+maiswan@users.noreply.github.com>
2025-07-23 13:15:49 +0800

    More robust IP wildcard matching (provided by ip-matching)

--- server/src/checkApiAccess.ts
+++ server/src/checkApiAccess.ts
@@ -1,20 +1,18 @@
 import { Request, Response, NextFunction } from "express";
+import { matches } from "ip-matching";
 
 export default function checkApiAccess(req: Request, res: Response, next: NextFunction, apiFullAllowedHosts: string[], apiBaseAllowedHosts: string[]) {
     const ip = req.ip ?? "";
-    const canAccessFullApi = apiFullAllowedHosts.includes("*") || apiFullAllowedHosts.includes(ip);
-    const canAccessBaseApi = apiBaseAllowedHosts.includes("*") || apiBaseAllowedHosts.includes(ip);
+    const canAccessFullApi = apiFullAllowedHosts.find(x => matches(ip, x));
+    const canAccessBaseApi = apiBaseAllowedHosts.find(x => matches(ip, x));
     const isAccessingFullApi = req.path.includes("commands") && req.method !== "POST";
 
-    if (!canAccessFullApi && isAccessingFullApi) {
+    const canAccess = !canAccessBaseApi || !canAccessFullApi && isAccessingFullApi;
+
+    if (!canAccess) {
         res.status(403).send();
         return;
     }
-
-    if (!canAccessBaseApi) {
-        res.status(403).send();
-        return;
-    }
-
+    
     next();
 }
