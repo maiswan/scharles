82d736eef5511bb7351e9b0d647bb7dbbdee0582
maiswan <173935047+maiswan@users.noreply.github.com>
2025-08-10 19:57:04 +0800

    Fix input length limit since we don't put data in the query path anymore

--- server/src/createApp.ts
+++ server/src/createApp.ts
@@ -7,20 +7,14 @@ import { ServerConfig } from "../config";
 export default function createApp(config: ServerConfig) {
     // Initialize server
     const app = express();
-    app.use(express.json());
     app.use(cors());
     
     // File-based routing
     (async () => { app.use("/", await router()); })();
     
-    // Limit path length
-    app.use((req: Request, res: Response, next: NextFunction): void => {
-        if (req.path.length > config.maxCommandLength) {
-            res.status(414).send("Command too long.");
-            return;
-        }
-        next();
-    });
+    // Limit body length
+    app.use(express.json({ limit: config.maxCommandLength }));
+    app.use(express.urlencoded({ extended: true }));
     
     // Limit rate
     app.use(rateLimit({
