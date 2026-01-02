1458b178c6095cad13aee242c780736532f06c9e
maiswan <173935047+maiswan@users.noreply.github.com>
2026-01-02 17:13:00 +0800

    Use state variables instead of methods to access shared values

--- client/src/hooks/WebSocketHandlers/handlers/ServerMessageHandler.ts
+++ client/src/hooks/WebSocketHandlers/handlers/ServerMessageHandler.ts
@@ -1,15 +1,15 @@
-import { type ConfigKey } from "../../ConfigurationContext";
 import { ServerMessage } from "../../../../../shared/ServerMessage";
 import { useLogger } from "../../useLogger";
 import { ClientMessage } from "../../../../../shared/ClientMessage";
 import { CommandAPI } from "../../CommandBus";
+import { Config } from "../../ConfigurationContext";
 
 export type ServerMessageHandlerContext = {
-    getJwt: () => string | null,
-    getConfig: (key: ConfigKey) => string,
+    token: string | undefined,
+    config: Config,
     logger: ReturnType<typeof useLogger>,
-    send: (message: ClientMessage) => void;
-    dispatchCommand: (commandAPI: CommandAPI) => void;
+    send: (message: ClientMessage) => void,
+    dispatchCommand: (commandAPI: CommandAPI) => void,
 }
 
 export type ServerMessageHandler = 
