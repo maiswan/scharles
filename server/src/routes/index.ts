7131020aaf0ca252beae99a5dc239e11a931bf9e
maiswan <173935047+maiswan@users.noreply.github.com>
2026-01-04 11:03:34 +0800

    Expose current version as well

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
+import PackageJson from "../../package.json";
 
 export const get = async (req: Request, res: Response) => {
-  return res.status(200).send("scharles/server");
+    return res.status(200).json({
+        program: PackageJson.name,
+        author: "maiswan",
+        version: PackageJson.version,
+    });
 }
